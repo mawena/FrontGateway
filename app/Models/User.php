bf4ed0c0ccf7e4187b8b983c1dc08cad8a76b1b8
mawena <gamligocharles@gmail.com>
2024-11-16 21:23:48 +0000

    Model.Update: User: add events hasMany relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -47,4 +48,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+	public function events(): HasMany{
+		return $this->hasMany(Event::class, 'user_id', 'id');
+	}
 }
