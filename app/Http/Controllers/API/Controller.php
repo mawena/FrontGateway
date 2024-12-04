711c47ddecbfd66006e334f33a8bc82dbaf87337
mawena <gamligocharles@gmail.com>
2024-12-04 21:40:24 +0000

    Scribe.Regenerate

--- app/Http/Controllers/API/Controller.php
+++ app/Http/Controllers/API/Controller.php
@@ -20,7 +20,7 @@ class Controller extends BaseController
 	protected string $modelClass = "";
 
 	//Index, show and destroy
-	protected string $indexAbilityName = "viewAny";
+	protected string|null $indexAbilityName = "viewAny";
 	protected $indexManualFilter = null;
 	protected array $indexSearchFieldList = [];
 
