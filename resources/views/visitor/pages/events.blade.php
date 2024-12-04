6645c1b53139b319fce5416b0b4521cec928f814
mawena <gamligocharles@gmail.com>
2024-12-04 20:06:42 +0000

    Scribe.Regenerate

--- resources/views/visitor/pages/events.blade.php
+++ resources/views/visitor/pages/events.blade.php
@@ -20,11 +20,13 @@
         <br>
         <div class="slider-area">
             <div class="row justify-content-center gx-3 gy-4">
-				@foreach ($events as $key => $event)
+				@forelse ($events as $event)
 					<div class="col-lg-6 col-xl-6 col-md-12 pb-3">
 						@include('visitor.modules.event')
 					</div>
-				@endforeach
+				@empty
+					Aucun événement
+				@endforelse
             </div>
         </div>
         <div class="shape-mockup shape1 d-none d-xxl-block" data-bottom="20%" data-left="-17%"><img
