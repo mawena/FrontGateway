77c6b9091412b6757a7bab5d90a0682041f8ebab
mawena <gamligocharles@gmail.com>
2024-12-14 05:02:53 +0000

    Scribe.Regenerate

--- database/seeders/ConfigurationSeeder.php
+++ database/seeders/ConfigurationSeeder.php
@@ -14,10 +14,11 @@ class ConfigurationSeeder extends Seeder
     public function run(): void
     {
         Configuration::create(["key" => "Prix Unitaire", "value" => "25"]);
-        Configuration::create(["key" => "Token API", "value" => ""]);
-        Configuration::create(["key" => "Lien API POST", "value" => ""]);
-        Configuration::create(["key" => "Lien API Callback", "value" => ""]);
-        Configuration::create(["key" => "API SITE_SID", "value" => ""]);
-        Configuration::create(["key" => "Le lien de retour", "value" => ""]);
+        Configuration::create(["key" => "Token API", "value" => "928662274675c069963aab7.58937288"]);
+        Configuration::create(["key" => "Lien API POST", "value" => "https://api-checkout.cinetpay.com/v2/payment"]);
+        Configuration::create(["key" => "Lien API Callback", "value" => "https://pecorator.chawena.com/api/payment/callback"]);
+        Configuration::create(["key" => "API SITE_SID", "value" => "5884195"]);
+        Configuration::create(["key" => "API SECRET_KEY", "value" => "1261551590675c0763b702c3.64362583"]);
+        Configuration::create(["key" => "Le lien de retour", "value" => "https://pecorator.chawena.com/admin/payment"]);
     }
 }
