d16112560632379db99e5ee172f432c656d8083e
mawena <gamligocharles@gmail.com>
2025-01-29 20:52:58 +0000

    block clic droit

--- resources/views/visitor/base.blade.php
+++ resources/views/visitor/base.blade.php
@@ -48,8 +48,7 @@
 	@include('visitor.partials.navbar')
 	@yield('base.body')
 	@include('visitor.partials.footer')
-	<div class="scroll-top"><svg class="progress-circle svg-content" width="100%" height="100%"
-			viewBox="-1 -1 102 102">
+	<div class="scroll-top"><svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
 			<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
 				style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;">
 			</path>
@@ -70,6 +69,11 @@
 	<script src="{{ asset('/visitor/assets/js/nice-select.min.js') }}"></script>
 	<script src="{{ asset('/visitor/assets/js/main.js') }}"></script>
 	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+	<script>
+		document.addEventListener("contextmenu", function(event) {
+			event.preventDefault();
+		});
+	</script>
 	@stack('scripts')
 
 </body>
