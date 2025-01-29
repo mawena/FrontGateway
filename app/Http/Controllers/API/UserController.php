913a592ae81c0795a77dc20964cc89c08a761e1b
mawena <gamligocharles@gmail.com>
2025-01-29 20:20:22 +0000

    fix bug

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -183,7 +183,7 @@ class UserController extends Controller
 					"promoter.structure" => "required|min:2",
 					"promoter.phone_number" => "required|min:2",
 					"promoter.birth_date" => "required|date",
-					"promoter.sex" => "required|in:M,F",
+					"promoter.sex" => "required|in:M,H",
 				],);
 				if ($validator->fails()) {
 					return ["errors" => $validator->errors()->toArray(), "status" => 400];
