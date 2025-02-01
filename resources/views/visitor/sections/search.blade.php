6c214e577f5d66bd2c748282eb46250a1ea12d29
mawena <gamligocharles@gmail.com>
2025-02-01 09:58:43 +0000

    fix part search

--- resources/views/visitor/sections/search.blade.php
+++ resources/views/visitor/sections/search.blade.php
@@ -1,60 +1,22 @@
 <div class="booking-sec">
-    <div class="container mb-0">
-        <form action="{{ $_link }}" method="GET" class="booking-form">
-            <div class="input-wrap">
-                <div class="row align-items-center justify-content-between">
-                    <div class="form-group col-md-12 col-lg-auto">
-                        <div class="icon"><i class="fa-light fa-route"></i></div>
-                        <div class="search-input">
-                            <label>Lieu</label>
-                            <select required name="place" id="place"
-                                class="form-select nice-select">
-                                <option selected value="" disabled>
-                                    * Choisir un lieu
-                                </option>
-                                <option @selected(request('place') == "Lomé") value="Lomé">Lomé</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-12 col-lg-auto">
-                        <div class="icon"><i class="fa-regular fa-person-hiking"></i></div>
-                        <div class="search-input">
-                            <label>Modalité de participation</label>
-                            <select required class="nice-select" name="type" id="type">
-                                <option selected value="" disabled>
-                                   * Choisir une modalité
-                                </option>
-                                <option @selected(request('type') == "free") value="free">Participation Gratuite</option>
-                                <option @selected(request('type') == "paid") value="paid">Participation Payante</option>
-                            </select>
-                        </div>
-                    </div>
-                    {{-- <div class="form-group col-md-6 col-lg-auto">
-                        <div class="icon"><i class="fa-light fa-clock"></i></div>
-                        <div class="search-input">
-                            <label>Intervalle du</label>
-                            <input required type="date" value="{{ request('start') ?? \Carbon\Carbon::now()->toDateString() }}"
-                                class="form-input" name="start" />
-                        </div>
-                    </div>
-                    <div class="form-group col-md-6 col-lg-auto">
-                        <div class="icon"><i class="fa-light fa-clock"></i></div>
-                        <div class="search-input">
-                            <label>Au</label>
-                            <input required type="date"
-                                value="{{ request('end') ?? \Carbon\Carbon::now()->addMonth()->toDateString() }}" class="form-input"
-                                name="end" />
-                        </div>
-                    </div> --}}
-                    <div class="form-btn col-md-12 col-lg-auto">
-                        <button type="submit" class="th-btn">
-                            <img src="{{ asset('/visitor/assets/img/icon/search.svg') }}" alt="">
-                            Rechercher
-                        </button>
-                    </div>
-                </div>
-                <p class="form-messages mb-0 mt-3"></p>
-            </div>
-        </form>
-    </div>
+	<div class="container mb-0">
+		<form action="{{ $_link }}" method="GET" class="booking-form">
+			<div class="input-wrap">
+				<div class="row align-items-center justify-content-between">
+					<div class="form-group col-10">
+						<div class="search-input">
+							<input class="form-control" type="text" name="search" placeholder="Chercher des événements" />
+						</div>
+					</div>
+					<div class="form-btn col-2">
+						<button type="submit" class="th-btn">
+							<img src="{{ asset('/visitor/assets/img/icon/search.svg') }}" alt="Rechercher">
+							Rechercher
+						</button>
+					</div>
+				</div>
+				<p class="form-messages mb-0 mt-3"></p>
+			</div>
+		</form>
+	</div>
 </div>
