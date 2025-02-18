80dcff5d6e2accb5c44ee02ba00380b3bffcd985
mawena <gamligocharles@gmail.com>
2025-02-18 22:01:43 +0000

    working

--- app/Http/Controllers/API/Controller.php
+++ app/Http/Controllers/API/Controller.php
@@ -73,7 +73,6 @@ class Controller extends BaseController
 		if ($this->indexManualFilter) {
 			$list = ($this->indexManualFilter)($list, $connectedUser);
 		}
-		dd($list->get());
 		return $this->responseIndexOk($list, $requestData, $this->modelName);
 	}
 
